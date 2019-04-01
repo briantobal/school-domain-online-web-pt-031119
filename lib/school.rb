9e886f7bc0fc389e0b5ed3eddd8d986ac497e6da
Brian Tobal <brian.tobal@gmail.com>
2019-04-01 09:58:11 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,8 +1,13 @@
 # code here!
 
 class School 
-  attr_accessor :roster 
+  attr_accessor :roster :student 
   
   roster = {}
+  
+  def add_student(student)
+    student = @student 
+    roster << student 
+  end
   
 end 
